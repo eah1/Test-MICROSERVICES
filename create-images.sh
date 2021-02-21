47e3086e7c5c306cd2dd5f9418ccd2053a244d14
Eduardo Arnedo Hidalgo <eduardoarnedo90@gmail.com>
2021-02-21 20:37:23 +0100

    Create files docker to create image Java micro

--- create-images.sh
+++ create-images.sh
@@ -6,6 +6,11 @@
 # Description: Create images docker microservice Java and Go
 ########################################################################################
 
+# Microservice Java
+mvn -f Image1/pom.xml clean package
+docker-compose -f Image1/docker-compose.yml build
+docker-compose -f Image1/docker-compose.yml down
+docker-compose -f Image1/docker-compose.yml up -d
 # Microservice Golang
 docker-compose -f Image2/docker-compose.yml build
 docker-compose -f Image2/docker-compose.yml down
