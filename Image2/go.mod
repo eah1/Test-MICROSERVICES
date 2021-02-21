5eabcb59c689629a43eac9beeb39ba8efe37172f
Eduardo Arnedo Hidalgo <eduardoarnedo90@gmail.com>
2021-02-21 20:17:30 +0100

    Add swagger in micro Golang

--- Image2/go.mod
+++ Image2/go.mod
@@ -3,11 +3,20 @@ module Test-MICROSERVICES/Image2
 go 1.15
 
 require (
-	github.com/gin-contrib/sse v0.0.0-20190125020943-a7658810eb74 // indirect
-	github.com/gin-gonic/gin v1.3.0
+	github.com/alecthomas/template v0.0.0-20190718012654-fb15b899a751
+	github.com/cpuguy83/go-md2man/v2 v2.0.0 // indirect
+	github.com/gin-gonic/gin v1.4.0
+	github.com/go-openapi/spec v0.20.3 // indirect
 	github.com/golang/protobuf v1.4.3 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/swaggo/gin-swagger v1.3.0
+	github.com/swaggo/swag v1.7.0
 	github.com/ugorji/go v1.2.4 // indirect
+	golang.org/x/net v0.0.0-20210220033124-5f55cee0dc0d // indirect
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
+	golang.org/x/tools v0.1.0 // indirect
 	gopkg.in/go-playground/validator.v8 v8.18.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- Image2/src/main.go
+++ Image2/src/main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
-	"net/http"
-
+	_ "Test-MICROSERVICES/Image2/src/docs"
 	"github.com/gin-gonic/gin"
+	"github.com/swaggo/gin-swagger"
+	"github.com/swaggo/gin-swagger/swaggerFiles"
+	"net/http"
 )
 
+// @title User API
+// @description User microservice server.
+// @schemes http https
+// @securityDefinitions.basic BasicAuth
 func main() {
 
 	r := gin.Default()
 
-	r.GET("/health", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
+	// ListUser is the handler of list user endpoint
+	// @Summary Health
+	// @Description Endpoint health
+	// @Tags health
+	// @Produce  json
+	// @Success 200 type {string}
+	// @Router /health [get]
+	r.GET("/health", checkHandler())
+
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
+
+	r.Run(":8020")
+}
+
+// CheckHandler returns an HTTP handler to perform health checks.
+// ListUser is the handler of list user endpoint
+// @Summary Health
+// @Description Endpoint health
+// @Tags health
+// @Produce  json
+// @Success 200 type {string}
+// @Router /health [get]
+func checkHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
 			"status": "Ok!",
 		})
-	})
-
-	r.Run(":8020") // listen and serve on 0.0.0.0:8080
+	}
 }
